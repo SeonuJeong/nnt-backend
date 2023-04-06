663c6230f0cd56e7f76857672e61b8639ce88461
Jeong Seon U <jung48273@gmail.com>
2023-04-06 13:53:58 +0900

    feat: add Basic Query method to UserRepository

--- nonata/src/main/java/com/nonata/domain/repository/UserRepository.java
+++ nonata/src/main/java/com/nonata/domain/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package com.nonata.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nonata.domain.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	Optional<User> findByEmail(String email);
+
+    Optional<User> findByNickname(String nickname);
+    
 }
