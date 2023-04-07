b8885e112018ffff52b4ea0a455df21538db2dd6
Jeong Seon U <jung48273@gmail.com>
2023-04-07 23:43:42 +0900

    feat: add findByHost method to RoomRepository

--- nonata/src/main/java/com/nonata/domain/repository/RoomRepository.java
+++ nonata/src/main/java/com/nonata/domain/repository/RoomRepository.java
@@ -1,9 +1,14 @@
 package com.nonata.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nonata.domain.model.Room;
+import com.nonata.domain.model.User;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
-
+	
+	Optional<Room> findByHost(User host);
+	
 }
